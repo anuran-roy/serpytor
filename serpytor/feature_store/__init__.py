3eeb2dba4ebb220d0999086ec2397ec9620e9fcd
Anuran Roy <76481787+anuran-roy@users.noreply.github.com>
2022-10-25 19:12:31 +0530

    Added module-level docstrings

--- serpytor/feature_store/__init__.py
+++ serpytor/feature_store/__init__.py
@@ -1,5 +1,11 @@
-from .feature_stores import SimpleFeatureStore
-from .config import CONFIG
+from serpytor.feature_store.feature_stores import SimpleFeatureStore
+from serpytor.feature_store.config import CONFIG
+
+"""
+This module contains the implementations for the different feature stores to be supported.  
+Currently, the only supported feature store is ``SimpleFeatureStore``.
+"""
+
 
 __all__ = (
     "SimpleFeatureStore",
