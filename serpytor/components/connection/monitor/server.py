e28b8e1d2ce9caf3762d2b450d2752267a816d91
Anuran Roy <anuranroy02@gmail.com>
2023-01-17 10:31:38 +0530

    Added code executor

--- serpytor/components/connection/monitor/server.py
+++ serpytor/components/connection/monitor/server.py
@@ -8,8 +8,10 @@ from tinydb import TinyDB, Query
 from serpytor.components.connection.monitor.utils import detect_ip
 from typing import Optional, Callable, Tuple, Union, List, Dict, Any
 from multiprocessing import Process
+import multiprocessing as mp
 import os
 import psutil
+from io import StringIO, BytesIO
 
 
 class ForbiddenDeviceException(Exception):
@@ -19,9 +21,9 @@ class ForbiddenDeviceException(Exception):
 
 # class HeartbeatServer:
 #     def __init__(self, hostName: str = "0.0.0.0", port: int = 6666) -> None:
-#         self.port: int = port
+#         self.heartbeat_port: int = port
 #         self.type = "Server"
-#         self.hostName: str = hostName
+#         self.heartbeat_hostName: str = hostName
 #         self.app = web.Application()
 #         self.config = safe_load(open("./monitor_config.yml", "r").read())
 #         self.routes = [web.get("/", self.handleRequest)]
@@ -54,9 +56,9 @@ class ForbiddenDeviceException(Exception):
 #         )
 
 #     def listen(self) -> None:
-#         print(f"Server started http://{self.hostName}:{self.port}")
+#         print(f"Server started http://{self.heartbeat_hostName}:{self.heartbeat_port}")
 #         self.add_all_routes()
-#         web.run_app(self.app, host=self.hostName, port=self.port)
+#         web.run_app(self.app, host=self.heartbeat_hostName, port=self.heartbeat_port)
 
 #     def stop_server(self) -> None:
 #         self.app.shutdown()
@@ -73,16 +75,16 @@ class ForbiddenDeviceException(Exception):
 class HeartbeatServer:
     def __init__(
         self,
-        mappings: Dict[str, Dict[str, Union[str, Callable]]],
-        port: Optional[int] = 8000,
-        host: Optional[str] = "localhost",
-        server_args: List = [],
-        server_kwargs: Optional[Dict[str, Any]] = {},
+        heartbeat_mappings: Dict[str, Dict[str, Union[str, Callable]]],
+        heartbeat_port: Optional[int] = 8000,
+        heartbeat_host: Optional[str] = "127.0.0.1",
+        heartbeat_server_args: List = [],
+        heartbeat_server_kwargs: Optional[Dict[str, Any]] = {},
         *args,
         **kwargs,
     ) -> None:
         """
-        Initializes the HeartbeatServer class with the mappings.
+        Initializes the HeartbeatServer class with the heartbeat_mappings.
 
         Format for mapping:
 
@@ -101,49 +103,157 @@ class HeartbeatServer:
 
         If an endpoint type is not specified, defaults to "get"
         """
-        self.mappings: List[Tuple[str, Callable]] = mappings.items()
-        self.web_server = web.Application()
-        self.port = port
-        self.host = host
-        self.server_kwargs = server_kwargs
-        self.server_args = server_args
+        self.heartbeat_mappings: List[Tuple[str, Callable]] = heartbeat_mappings.items()
+        self.heartbeat_web_server = web.Application()
+        self.heartbeat_port: int = heartbeat_port
+        self.heartbeat_host: str = heartbeat_host
+        self.heartbeat_server_kwargs: Dict[str, Any] = heartbeat_server_kwargs
+        self.heartbeat_server_args: List[Any] = heartbeat_server_args
+        self.resource_online: bool = False
 
-    def transform_mappings(self):
-        transformed_mappings = []
+    def transform_mappings(
+        self, mappings: Dict[str, Dict[str, Union[str, Callable]]]
+    ) -> List[Callable]:
+        transformed_heartbeat_mappings: List[Callable] = []
 
-        for i in self.mappings:
+        for i in mappings:
             request_type = i[1].get("type", "get")
 
             if request_type == "get":
-                transformed_mappings.append(web.get(i[0], i[1]["mapped_method"]))
+                transformed_heartbeat_mappings.append(
+                    web.get(i[0], i[1]["mapped_method"])
+                )
             elif request_type == "post":
-                transformed_mappings.append(web.post(i[0], i[1]["mapped_method"]))
+                transformed_heartbeat_mappings.append(
+                    web.post(i[0], i[1]["mapped_method"])
+                )
             elif request_type == "put":
-                transformed_mappings.append(web.put(i[0], i[1]["mapped_method"]))
+                transformed_heartbeat_mappings.append(
+                    web.put(i[0], i[1]["mapped_method"])
+                )
             elif request_type == "patch":
-                transformed_mappings.append(web.patch(i[0], i[1]["mapped_method"]))
+                transformed_heartbeat_mappings.append(
+                    web.patch(i[0], i[1]["mapped_method"])
+                )
             elif request_type == "delete":
-                transformed_mappings.append(web.delete(i[0], i[1]["mapped_method"]))
+                transformed_heartbeat_mappings.append(
+                    web.delete(i[0], i[1]["mapped_method"])
+                )
 
-        return transformed_mappings
+        return transformed_heartbeat_mappings
 
-    def start_heartbeats(self):
-        transformed_mappings = self.transform_mappings()
-        self.web_server.add_routes(transformed_mappings)
-        web.run_app(
-            self.web_server,
-            port=self.port,
-            host=self.host,
-            *self.server_args,
-            **self.server_kwargs,
+    def start_heartbeats(self) -> None:
+        transformed_heartbeat_mappings = self.transform_mappings(
+            self.heartbeat_mappings
         )
+        self.heartbeat_web_server.add_routes(transformed_heartbeat_mappings)
+        # try:
+        heartbeat_process = Process(
+            target=web.run_app,
+            args=(*self.heartbeat_server_args,),
+            kwargs={
+                "app": self.heartbeat_web_server,
+                "port": self.heartbeat_port,
+                "host": self.heartbeat_host,
+                **self.heartbeat_server_kwargs,
+            },
+        )
+        print(
+            f"Starting heartbeat server at {self.heartbeat_host}:{self.heartbeat_port}"
+        )
+        self.resource_online = True
+        heartbeat_process.start()
+        # print(
+        #     f"Stopping heartbeat server at {self.heartbeat_host}:{self.heartbeat_port}"
+        # )
+        # heartbeat_process.join()
+        # self.resource_online = False
+        # except Exception as e:
+        # print(f"Server {self.__str__} crashed :(")
+        # self.resource_online = False
+
+
+class Server(HeartbeatServer):
+    def __init__(
+        self,
+        mappings: Dict[str, Dict[str, Union[str, Callable]]],
+        server_port: int,
+        server_host: str,
+        *args: List[Any],
+        **kwargs: Dict[str, Any],
+    ) -> None:
+        super().__init__(*args, **kwargs)
+        self.mappings: List[Tuple[str, Callable]] = mappings.items()
+        self.service_web_server = web.Application()
+        self.server_metadata: Dict[str, str] = {
+            "name": kwargs.get("server_name", "SerPyTor Server"),
+            "spawned_on": kwargs.get("spawned_on", str(datetime.now())),
+        }
+        self.server_port: int = server_port
+        self.server_host: str = server_host
+        self.process_lock = mp.Lock()
+        self.service_online: bool = True
+
+    def __str__(self):
+        return "Server"
+
+    def add_mappings(
+        self, mappings: Dict[str, Dict[str, Union[str, Callable]]]
+    ) -> None:
+        """Transforms the given endpoint definitions.
+        This changes them to their respective route commands.
+        This means less hassle to add endpoints to the web server
+        """
+
+    def start_service(
+        self,
+        *server_args,
+        **server_kwargs,
+    ) -> None:
+        """Starts the service on the specified port and host.
+        Uses a separate process to do the same so that a single error doesn't crash the entire server.
+        """
+        transformed_mappings: List[Callable] = self.transform_mappings(self.mappings)
+        self.service_web_server.add_routes(transformed_mappings)
+
+        try:
+            p = Process(
+                target=web.run_app,
+                # args=(self.service_web_server),
+                kwargs={
+                    "app": self.service_web_server,
+                    "port": self.server_port,
+                    "host": self.server_host,
+                    **server_kwargs,
+                },
+            )
+            print(
+                f"Starting service web server at {self.server_host}:{self.server_port}..."
+            )
+            self.service_online = True
+            p.start()
+        # print(f"Exiting service web server at {self.server_host}:{self.server_port}...")
+        # p.join()
+        # self.service_online = False
+        except Exception as e:
+            print(f"Service {self.__str__} crashed :(")
+            self.service_online = False
+
+    def is_service_online(self) -> bool:
+        return self.service_online
+
+    def execute(self) -> None:
+        heartbeat_process = Process(target=self.start_heartbeats)
+        service_process = Process(target=self.start_service)
+        heartbeat_process.start()
+        service_process.start()
 
 
 if __name__ == "__main__":
     # HeartbeatServer().execute()
     # print(detect_ip())
 
-    async def handlePost(request):
+    async def handleGet(request):
         """Contains the mapping method that will print the IP of the request origin.
         It also returns the cpu and memory usage of the system the server instance is running on.
         """
@@ -159,23 +269,103 @@ if __name__ == "__main__":
             content_type="text/json",
         )
 
-    mapping = {"/heartbeat": {"type": "get", "mapped_method": handlePost}}
+    async def hello(request):
+        print("Hello!")
+        return web.Response(
+            text=json.dumps({"message": "Hello!", "target": request.remote}),
+            content_type="text/json",
+        )
+
+    def sanity_check(code, args, kwargs):
+        """Checks if the code is safe to execute.
+        This is done by checking if the code is a function, and if the args and kwargs are of the correct type.
+        """
+        # return (
+        #     isinstance(code, types.FunctionType)
+        #     and isinstance(args, tuple)
+        #     and isinstance(kwargs, dict)
+        # )
+
+        return True
+
+    async def exec_func(
+        request, sanity_checking: Callable[..., bool] = sanity_check
+    ) -> Any:
+        import pickle
+
+        """Receives a chunk of code (complete with imports, etc), executes it, and returns an output."""
+        reader = await request.multipart()
+        code_field = await reader.next()
+        code = await code_field.read()
+        args_field = await reader.next()
+        args = await args_field.read()
+        kwargs_field = await reader.next()
+        kwargs = await kwargs_field.read()
+
+        check_complete: bool = sanity_checking(
+            pickle.loads(code),
+            pickle.loads(args),
+            pickle.loads(kwargs),
+        )
+        message: str = "Sanity check not passed."
+        output: Union[Any, None] = None
+        if check_complete:
+            output = pickle.loads(code)(*pickle.loads(args), **pickle.loads(kwargs))
+            message = "Sanity check passed."
+        print("Computation complete.")
+        return web.Response(
+            text=json.dumps({"message": message, "output": output}),
+            content_type="text/json",
+        )
+
+    heartbeat_mapping: Dict[str, Dict[str, Union[str, Callable[..., Any]]]] = {
+        "/heartbeat": {"type": "get", "mapped_method": handleGet}
+    }
+    mapping: Dict[str, Dict[str, Union[str, Callable[..., Any]]]] = {
+        "/hello": {"type": "get", "mapped_method": hello},
+        "/exec": {"type": "post", "mapped_method": exec_func},
+    }
 
     def test_example_v2():
-        server1 = HeartbeatServer(mappings=mapping, port=5000)
+        server1 = Server(
+            heartbeat_mappings=heartbeat_mapping,
+            heartbeat_port=5000,
+            mappings=mapping,
+            server_host="127.0.0.1",
+            server_port=8100,
+        )
         # server1.start_heartbeats()
-        server2 = HeartbeatServer(mappings=mapping, port=5001)
+        server2 = Server(
+            heartbeat_mappings=heartbeat_mapping,
+            heartbeat_port=5001,
+            mappings=mapping,
+            server_host="127.0.0.1",
+            server_port=8101,
+        )
         # server2.start_heartbeats()
-        server3 = HeartbeatServer(mappings=mapping, port=5002)
+        server3 = Server(
+            heartbeat_mappings=heartbeat_mapping,
+            heartbeat_port=5002,
+            mappings=mapping,
+            server_host="127.0.0.1",
+            server_port=8102,
+        )
         # server3.start_heartbeats()
-        server4 = HeartbeatServer(mappings=mapping, port=5003)
+        server4 = Server(
+            heartbeat_mappings=heartbeat_mapping,
+            heartbeat_port=5003,
+            mappings=mapping,
+            server_host="127.0.0.1",
+            server_port=8103,
+        )
         # server4.start_heartbeats()
 
         servers = [server1, server2, server3, server4]
         processes = []
 
-        for i in servers:
-            processes.append(Process(target=i.start_heartbeats))
+        for idx, i in enumerate(servers):
+            print(f"Spawning server {idx+1}")
+            processes.append(Process(target=i.execute))
 
         for i in processes:
             i.start()
