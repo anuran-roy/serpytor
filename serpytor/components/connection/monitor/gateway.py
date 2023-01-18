1e265d324a306cd3fb3bb229af8dead039c2953b
Anuran Roy <anuranroy02@gmail.com>
2023-01-18 15:37:44 +0530

    [FIX] Fixed work distriubtion among gateway resources

--- serpytor/components/connection/monitor/gateway.py
+++ serpytor/components/connection/monitor/gateway.py
@@ -1,6 +1,6 @@
 import asyncio
 from typing import Any, Callable, Dict, List, Optional, Tuple
-
+import requests
 import aiohttp
 import cloudpickle
 
@@ -25,7 +25,7 @@ class Gateway:
         self,
         task: Callable[..., Any],
         allocation_algorithm: BaseAllocation,
-        task_data: Optional[Tuple[List[Any], Dict[str, Any]]] = ([], {}),
+        task_setup_data: Optional[Tuple[List[Any], Dict[str, Any]]] = ([], {}),
         heartbeat_addresses: Optional[List[str]] = [],
         resource_addresses: Optional[List[str]] = [],
         *args: Optional[List[Any]],
@@ -34,7 +34,7 @@ class Gateway:
         self._heartbeat_addr: List[str] = heartbeat_addresses
         self._resource_addr: List[str] = resource_addresses
         self._task: Callable[..., Any] = task
-        self._task_input: Tuple[List[Any], Dict[str, Any]] = task_data
+        self._task_setup_input: Tuple[List[Any], Dict[str, Any]] = task_setup_data
         self._allocation_algorithm: BaseAllocation = allocation_algorithm
 
     def __str__(self) -> str:
@@ -43,55 +43,88 @@ class Gateway:
     def __repr__(self) -> str:
         return f"Gateway({self._task.__name__}) with {len(self._resource_addr)} resources at {self._resource_addr} using {self._allocation_algorithm.__class__.__name__} algorithm"
 
+    def change_task(self, task: Callable[..., Any]) -> None:
+        self._task = task
+
     async def get_available_resources(
         self, *args: Optional[List[Any]], **kwargs: Optional[Dict[str, Any]]
     ) -> Any:
+        """Get vitals report from webservers at any given time"""
+        print("Getting available resources...")
         report: Dict[str, Dict[str, Any]] = {addr: {} for addr in self._resource_addr}
         async with aiohttp.ClientSession(
-            conn_timeout=kwargs.get("timeout", 10.0)
+            # timeout=kwargs.get("timeout", 10.0)
         ) as session:
-            for resource_addr, heartbeat_addr in zip(
-                self._resource_addr, self._heartbeat_addr
+            print(f"Sending request to {len(self._resource_addr)} resources")
+            for idx, zipped_tuple in enumerate(
+                zip(self._resource_addr, self._heartbeat_addr)
             ):
+                resource_addr, heartbeat_addr = zipped_tuple
+                print(f"Sending request {idx}...")
                 async with session.get(heartbeat_addr) as resp:
                     report[resource_addr] = await resp.json()
 
         return report
 
-    def allocate_resource(
+    async def allocate_resource(
         self, *args: Optional[List[Any]], **kwargs: Optional[Dict[str, Any]]
     ) -> Any:
-        """Get report from webservers at any given time"""
-        report: Any = asyncio.run(self.get_available_resources())
+        """Get vital reports from webservers at any given time."""
+        report: Any = await self.get_available_resources()
+        print("Received resource reports. Forwarding to allocation algorithm...")
+        self._allocation_algorithm.put(report)
         optimal_resource: Any = self._allocation_algorithm.queue(
-            report, selection_criteria=kwargs.get("selection_criteria", "cpu")
+            selection_criteria=kwargs.get("selection_criteria", "cpu")
         )
+        print(optimal_resource)
         return optimal_resource
 
     async def execute(
-        self, *args: Optional[List[Any]], **kwargs: Optional[Dict[str, Any]]
+        self,
+        task_args: Optional[List[Any]] = [],
+        task_kwargs: Optional[Dict[str, Any]] = {},
+        *args: Optional[List[Any]],
+        **kwargs: Optional[Dict[str, Any]],
     ) -> Any:
-        resource_details = self.allocate_resource()
+        """Execute the task on the allocated resource.
+        We use sync requests here due to some problems with AIOHttp requests.
+        Moreover, for a single request, sync requests are faster than async requests.
+        """
+        resource_details = await self.allocate_resource()
         task_pickle = cloudpickle.dumps(self._task)
-        task_args, task_kwargs = self._task_input
-        args_pickle = cloudpickle.dumps(task_args)
-        kwargs_pickle = cloudpickle.dumps(task_kwargs)
+        task_setup_args, task_setup_kwargs = self._task_setup_input
+        args_pickle = cloudpickle.dumps(task_setup_args + task_args)
+        kwargs_pickle = cloudpickle.dumps(task_setup_kwargs | task_kwargs)
 
-        execution_loc: str = (
-            f"{resource_details['location']}/{kwargs.get('execution_endpoint', 'exec')}"
-        )
-        async with aiohttp.ClientSession(execution_loc) as session:
-            async with session.post(
-                execution_loc,
-                files={
-                    "code": task_pickle,
-                    "args": args_pickle,
-                    "kwargs": kwargs_pickle,
-                },
-            ) as resp:
-                return await resp.text()
+        execution_loc: str = f"{resource_details}"
 
+        print("Executing at", execution_loc)
         # Execute the task
+
+        # async with aiohttp.ClientSession(execution_loc) as session:
+        #     async with session.post(
+        #         execution_loc,
+        #         files={
+        #             "code": task_pickle,
+        #             "args": args_pickle,
+        #             "kwargs": kwargs_pickle,
+        #         },
+        #     ) as resp:
+        #         text = await resp.text()
+        #         print(text)
+        #         return text
+
+        res = requests.post(
+            execution_loc,
+            files={
+                "code": task_pickle,
+                "args": args_pickle,
+                "kwargs": kwargs_pickle,
+            },
+        )
+        json_res = res.json()
+        # print(json_res)
+        return json_res
 
 
 if __name__ == "__main__":
@@ -104,21 +137,22 @@ if __name__ == "__main__":
 
     # Create a gateway object
     gateway = Gateway(
-        task=lambda _: print("Hello World!"),
+        task=lambda: "Hello World!",
+        task_setup_data=([], {}),
         allocation_algorithm=FCFSAllocation(),
         resource_addresses=[
-            "http://localhost:8100/exec",
-            "http://localhost:8101/exec",
-            "http://localhost:8102/exec",
-            "http://localhost:8103/exec",
+            "http://127.0.0.1:8100/exec",
+            "http://127.0.0.1:8101/exec",
+            "http://127.0.0.1:8102/exec",
+            "http://127.0.0.1:8103/exec",
         ],
         heartbeat_addresses=[
-            "http://localhost:5000/heartbeat",
-            "http://localhost:5001/heartbeat",
-            "http://localhost:5002/heartbeat",
-            "http://localhost:5003/heartbeat",
+            "http://127.0.0.1:5000/heartbeat",
+            "http://127.0.0.1:5001/heartbeat",
+            "http://127.0.0.1:5002/heartbeat",
+            "http://127.0.0.1:5003/heartbeat",
         ],
     )
 
     # Execute the gateway
-    gateway.execute()
+    asyncio.run(gateway.execute())
