d1cf76537f166ab26caa0ace2f1ac9d25f0fb32b
Anuran Roy <anuranroy02@gmail.com>
2023-03-10 20:35:46 +0530

    [FIX] Fixed exception class code

--- serpytor/components/pipelines/exceptions.py
+++ serpytor/components/pipelines/exceptions.py
@@ -9,24 +9,24 @@ from serpytor.components.logging.exceptions import (
 
 class CriticalPipelineError(CriticalLog):
     def __init__(self, message: str) -> None:
-        super(CriticalPipelineError, self).__init__(message)
+        super().__init__(message)
 
 
 class PipelineError(ErrorLog):
     def __init__(self, message: str) -> None:
-        super(ErrorLog, self).__init__(message)
+        super().__init__(message)
 
 
 class PipelineWarning(WarningLog):
     def __init__(self, message: str) -> None:
-        super(WarningLog, self).__init__(message)
+        super().__init__(message)
 
 
 class PipelineInfo(InfoLog):
     def __init__(self, message: str) -> None:
-        super(InfoLog, self).__init__(message)
+        super().__init__(message)
 
 
 class PipelineDebugInfo(DebugLog):
     def __init__(self, message: str) -> None:
-        super(DebugLog, self).__init__(message)
+        super().__init__(message)
